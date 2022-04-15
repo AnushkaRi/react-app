5198db14d7108869f085e40999a359578ea5bb55
Ana Dejanović <ana.dejanovich@gmail.com>
2022-04-15 14:10:38 +0200

    Add Timer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { idHelpers } from "./library-react/helpers";
-import { RandomValue } from "./components/RandomValue";
 import { RepositoryLink } from "./components/RepositoryLink";
-import { ABQuestion } from "./components/ABQuestions";
+import { Quiz } from "./components/Quiz";
+import { useState } from "react";
+import { LoginForm } from "./components/LoginForm";
+import { Timer } from "./components/Timer";
 
 function App() {
-  const handleOnChoice = (choiceValue) => console.log(choiceValue);
+  const [actionCount, setActionCount] = useState(0);
+  const [quizAnswer, setQuizAnswer] = useState(null);
+  const [loginState, setLoginState] = useState(null);
+  const [finishTime, setFinishTime] = useState(null);
+
+  const id = idHelpers.generateId();
+
+  const handleSubmit = (answers, id) => setQuizAnswer({ answers, id });
+  const handleStateChange = () => setActionCount((state) => state + 1);
+  const handleLogin = (formState) => setLoginState(formState);
+  const handleTimerFinish = (time) => setFinishTime(time);
+
+  let answerComponents = null;
+
+  if (quizAnswer !== null) {
+    answerComponents = Object.keys(quizAnswer.answers).map((key) => {
+      return (
+        <div key={key}>
+          {key}: {quizAnswer.answers[key]}
+        </div>
+      );
+    });
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>algebra-example-react-app</h1>
-        <p>Your unique ID is: {idHelpers.generateId()}</p>
+        <p>Your action count is: {actionCount}</p>
         <RepositoryLink>View Repository</RepositoryLink>
-        <RandomValue values={[4, 5, 6]} />
-        <ABQuestion
-          question="Make the right choice"
-          buttonA="Blue pill"
-          buttonB="Red pill"
-          buttonAValue="Blue"
-          buttonBValue="Red"
-          onChoice={handleOnChoice}
-        />
+        {loginState === null && <LoginForm onLogin={handleLogin} />}
+        {loginState !== null && (
+          <div>
+            {loginState.name} ({loginState.email})
+          </div>
+        )}
+        {quizAnswer === null && loginState !== null && (
+          <>
+            <Timer onTick={(time) => console.log(time)} onFinish={handleTimerFinish} />
+            <Quiz id={id} onSubmit={handleSubmit} onStateChange={handleStateChange} />
+          </>
+        )}
+        {quizAnswer !== null && answerComponents}
+        {finishTime !== null && <div>{finishTime} s</div>}
       </header>
     </div>
   );
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,126 @@
+import { useEffect, useState } from "react";
+import { FreeInputQuestion } from "./FreeInputQuestion";
+import { SingleChoiceQuestion } from "./SingleChoiceQuestion";
+
+const questions = [
+  {
+    text: "Enter your name",
+    id: "name",
+  },
+  {
+    text: "Enter your age",
+    id: "age",
+  },
+  {
+    text: "Enter your favourite food",
+    id: "food",
+  },
+  {
+    text: "Enter your city",
+    id: "city",
+  },
+];
+
+const singleChoiceQuestions = [
+  {
+    id: "singleChoiceQuestion1",
+    question: "Make a choice:",
+    options: [
+      {
+        text: "Option A",
+        value: "A",
+        id: "option-a",
+      },
+      {
+        text: "Option B",
+        value: "B",
+        id: "option-b",
+      },
+      {
+        text: "Option C",
+        value: "C",
+        id: "option-c",
+      },
+    ],
+  },
+  {
+    id: "question1",
+    question: "Make the right choice:",
+    options: [
+      {
+        id: "blue-pill",
+        text: "Blue pill",
+        value: "Blue",
+      },
+      {
+        id: "red-pill",
+        text: "Red pill",
+        value: "Red",
+      },
+    ],
+  },
+  {
+    id: "question2",
+    question: "Make the right choice:",
+    options: [
+      {
+        id: "blue-pill",
+        text: "Blue pill",
+        value: "Blue",
+      },
+      {
+        id: "red-pill",
+        text: "Red pill",
+        value: "Red",
+      },
+      {
+        id: "pink-pill",
+        text: "Pink pill",
+        value: "Pink",
+      },
+    ],
+  },
+];
+
+export function Quiz(props) {
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    props.onStateChange();
+  }, [state]);
+
+  const handleAnswer = (id, choiceValue) => {
+    setState((currentState) => ({
+      ...currentState,
+      [id]: choiceValue,
+    }));
+  };
+
+  const handleSubmit = () => {
+    props.onSubmit(state, props.id);
+  };
+
+  const questionsMap = questions.map((question) => {
+    return <FreeInputQuestion key={question.id} id={question.id} text={question.text} onKeyUp={handleAnswer} />;
+  });
+
+  const singleChoiceQuestionsMap = singleChoiceQuestions.map((question) => {
+    return (
+      <SingleChoiceQuestion
+        key={question.id}
+        id={question.id}
+        question={question.question}
+        options={question.options}
+        onChoice={handleAnswer}
+      />
+    );
+  });
+
+  return (
+    <div>
+      {questionsMap}
+      {singleChoiceQuestionsMap}
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
